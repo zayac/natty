9809ebbc5aeec59845c3ddfb283be3c167733c6e
Pavel Zaichenkov <zaichenkov@frtk.ru>
2011-02-16 01:12:13 +0300

    Parser is modified. Comments for the Parser interface are created

--- src/main/java/ru/natty/parser/Parser.java
+++ src/main/java/ru/natty/parser/Parser.java
@@ -4,44 +4,41 @@
  */
 
 package ru.natty.parser;
-
-import java.net.MalformedURLException;
-import java.util.LinkedList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import jcifs.smb.SmbException;
-import jcifs.smb.SmbFile;
-import ru.natty.parser.files.MusicFile;
-
-
+import java.util.Map;
+import org.jaudiotagger.tag.Tag;
 
 /**
- *
- * @author zayac
+ * An interface that describes methods of a parser that runs over file system.
+ * The parser should start from the Home directory.
+ * All cached files information should be stored in the Tags structure,
+ * where the key is file's path, and Tag is for Tag in jAudioTagger library.
+ * @see org.jaudiotagger.tag.Tag
  */
-public class Parser {
-    public static void main(String[] args) throws SmbException {
-        SMBMusicController musicController = new SMBMusicController("smb://natalie.campus/music/");
-        musicController.startParsing();
-        
-//        LinkedList<String> fList = new LinkedList<String>();
-//	SmbFile f;
-//        try {
-//            f = new SmbFile("smb://natalie.campus/music/");
-//            SmbFile[] fArr = f.listFiles();
-//            for (int i= 0; i < fArr.length; i++)
-//                System.out.println(fArr[i].getName());
-//        } catch (MalformedURLException ex) {
-//            Logger.getLogger(Parser.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+public interface Parser {
+    /*
+     * Setter for Home path.
+     * Set Home directory, that will be used as the root directory for parser.
+     * "/" (slash) should end the path.
+     * @param a string that contains path, i.e. /home/musiclover/music/
+     */
 
-//        FileSystemManager fsManager;
-//        try {
-//            fsManager = VFS.getManager();
-//            FileObject sourceFile = fsManager.resolveFile("smb://natalie.campus/incom23fing");
-//
-//        } catch (FileSystemException ex) {
-//            Logger.getLogger(Parser.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-    }
+    public void setHome(String path);
+    /*
+     * Getter for Home path.
+     * @return a string that contains Home path
+     */
+    public String getHome();
+    /*
+     * Start parsing file system.
+     * Beware, you can't start parsing unless Home path is set.
+     * You can't access to Tags Map unless you have run this method.
+     */
+    public void parse();
+
+    /*
+     * Get Tags Map. Should be accessed after parse is run.
+     * @return A map where the key is for file absolute path and the value is for Tag from jAudioTagger library
+     * @see org.jaudiotagger.tag.Tag
+     */
+    public Map<String, Tag> getTags();
 }
